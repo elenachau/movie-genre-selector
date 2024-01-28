748d03661099292e186577454340a72a0e94003d
elenachau <elenaxcha@gmail.com>
2024-01-27 21:43:04 -0800

    implement movie.cpp

---
+++ movie.cpp
@@ -0,0 +1,115 @@
+#include "movie.h"
+
+Movie::Movie(){
+    title = "none";
+    director = "none";
+    description = "none";
+    actorCount = 0;
+    genreCount = 0;
+    year = 0;
+    metaScore = 0;
+    rating = 0.001;
+}
+
+Movie::Movie(string t, string d, string desc, string* a, string* g, int y, int meta, int ac, int gc, float rate) { //string* for an array of strings
+    title = t;
+    director = d;
+    description = desc;
+    actorCount = ac;
+    genreCount = gc;
+    year = y;
+    metaScore = meta;
+    rating = rate;
+
+    for(int i = 0; i < actorCount; i++) {
+        actors[i] = a[i];
+    }
+    for(int j = 0; j < genreCount; j++) {
+        genres[j] = g[j];
+    }
+}
+
+
+Movie::Movie(const Movie& rhs) { //reference to a Movie object
+    title = rhs.title;
+    director = rhs.director;
+    description = rhs.description;
+    actorCount = rhs.actorCount;
+    genreCount = rhs.genreCount;
+    year = rhs.year;
+    metaScore = rhs.metaScore;
+    rating = rhs.rating;
+
+    for(int i = 0; i < actorCount; i++) {
+        actors[i] = rhs.actors[i];
+    }
+    for(int j = 0; j < genreCount; j++) {
+        genres[j] = rhs.genres[j];
+    }
+}
+
+void Movie::setTitle(string t){
+    title = t;
+}
+
+void Movie::setDirector(string d){
+    director = d;
+}
+
+void Movie::setDescription(string desc){
+    description = desc;
+}
+
+void Movie::addGenre(string genre) {
+    genres[genreCount] = genre;
+}
+void Movie::setYear(int y) {
+    year = y;
+}
+
+void Movie::setMetaScore(int ms) {
+    metaScore = ms;
+}
+void Movie::setRating(float r) {
+    rating = r;
+}
+
+string Movie::getTitle() {
+    return title;
+}
+
+string Movie::getDirector() {
+    return director;
+}
+
+string Movie::getDescription() {
+    return description;
+}
+
+string* Movie::getActors() {
+    return actors;
+}
+
+string* Movie::getGenres() {
+    return genres;
+}
+
+int Movie::getYear() {
+    return year;
+}
+
+int Movie::getMetaScore() {
+    return metaScore;
+}
+
+int Movie::getActorCount() {
+    return actorCount;
+}
+
+int Movie::getGenreCount() {
+    return genreCount;
+}
+
+float Movie::getRating() {
+    return rating;
+}
