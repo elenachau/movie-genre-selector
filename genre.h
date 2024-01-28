90f5d76ce04ac0404df40d2aa531a5fbdd36bf7b
elenachau <elenaxcha@gmail.com>
2024-01-27 21:49:33 -0800

    implement genre.h

---
+++ genre.h
@@ -0,0 +1,26 @@
+#ifndef GENRE_H
+#define GENRE_H
+
+#include "movie.h"
+
+class Genre{
+    string genreOfObject;
+    Movie moviesOfGenre[1000];
+    int numMoviesInGenre;
+
+    public:
+        Genre();
+        Genre(string, Movie*, int);
+        Genre(const Genre&); //references a Genre object
+
+        void setGenreOfObject(string);
+        void addMovieToGenre(Movie);
+
+        string getGenreOfObject();
+        Movie* getMoviesofGenre();
+        int getNumMoviesInGenre();
+        void swapMovies(int, int);
+
+};
+
+#endif
