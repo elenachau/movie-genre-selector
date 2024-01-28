2b730f98af3fdcec18244d947a3a9e976f093646
elenachau <elenaxcha@gmail.com>
2024-01-27 22:14:45 -0800

    implement genre.cpp

---
+++ genre.cpp
@@ -0,0 +1,51 @@
+#include "genre.h"
+
+Genre::Genre(){
+    genreOfObject = "none";
+    numMoviesInGenre = 0;
+}
+
+Genre::Genre(string g, Movie* m, int n){
+    genreOfObject = g;
+    numMoviesInGenre = n;
+    for(int i = 0; i < n; i++){
+        moviesOfGenre[i] = m[i];
+    }
+}
+
+Genre::Genre(const Genre& rhs){
+    genreOfObject = rhs.genreOfObject;
+    numMoviesInGenre = rhs.numMoviesInGenre;
+    for(int i = 0; i < numMoviesInGenre; i++) {
+        moviesOfGenre[i] = rhs.moviesOfGenre[i];
+    }
+}
+
+
+void Genre::setGenreOfObject(string g){
+    genreOfObject = g;
+}
+
+void Genre::addMovieToGenre(Movie m){
+    moviesOfGenre[numMoviesInGenre] = m;
+    numMoviesInGenre++;
+}
+
+
+string Genre::getGenreOfObject(){
+    return genreOfObject;
+}
+
+Movie* Genre::getMoviesofGenre(){
+    return moviesOfGenre;
+}
+
+int Genre::getNumMoviesInGenre(){
+    return numMoviesInGenre;
+}
+
+void Genre::swapMovies(int i, int j){ //indices i and j
+    Movie temp = moviesOfGenre[i];
+    moviesOfGenre[i] = moviesOfGenre[j];
+    moviesOfGenre[j] = temp;
+}
